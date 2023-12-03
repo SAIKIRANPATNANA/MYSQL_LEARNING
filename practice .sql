564fba9c97ac2d4e8f665b3e913a8661115ae303
SAIKIRANPATANA <saikiranpatnana5143@gmail.com>
2023-12-03 10:34:45 +0000

    updated

--- practice .sql
+++ practice .sql
@@ -1,4 +1,4 @@
--- Active: 1700974372596@@127.0.0.1@3306
+-- Active: 1700974372596@@127.0.0.1@3306@mayya
 -- creating tables:
 create DATABASE mayya;
 use mayya;
@@ -1213,6 +1213,35 @@ select concat(first_name,last_name) as names,salary, 0.15*salary as pf from empl
 -- Write a query to select all record from employees where last name in 'BLAKE', 'SCOTT', 'KING' and 'FORD'.
 select * from employees where last_name like '%BLAKE%' or last_name like '%SCOTT%' or last_name like '%KING%' or last_name like '%FORD%';
 
+-- Write a query to list the number of jobs available in the employees table.
+select count(employee_id) from employees;
+--  Write a query to get the total salaries payable to employees.
+select sum(salary) from employees;
+--  Write a query to get the minimum salary from employees table.
+select min(salary) from employees;
+-- Write a query to get the maximum salary of an employee working as a Programmer.
+select max(salary) from employees;
+--  Write a query to get the average salary and number of employees working the department 90.
+select avg(salary), count(employee_id) from employees where department_id = 90;
+--  Write a query to get the highest, lowest, sum, and average salary of all employees.
+select max(salary), min(salary), sum(salary), avg(salary) from employees;
+--  Write a query to get the number of employees with the same job.
+select count(employee_id), job_id from employees group by job_id;
+--  Write a query to get the difference between the highest and lowest salaries.
+select max(salary)-min(salary) from employees;
+--  Write a query to find the manager ID and the salary of the lowest-paid employee for that manager.
+select manager_id,salary from employees where salary in (select min(salary) from employees);
+--  Write a query to get the department ID and the total salary payable in each department.
+select department_id,sum(salary) from employees group by  department_id;
+-- Write a query to get the average salary for each job ID excluding programmer.
+select job_id, avg(salary) from employees where job_id  not like '%prog%' group by job_id;
+--  Write a query to get the total salary, maximum, minimum, average salary of employees (job ID wise), for department ID 90 only.
+select job_id, sum(salary),max(salary),min(salary),avg(salary) from employees where DEPARTMENT_ID = 90 group by job_id;
+show tables;
+--  Write a query to get the job ID and maximum salary of the employees where maximum salary is greater than or equal to $4000.
+select job_id, max(salary) from employees where salary>=4000 GROUP BY job_id;
+--  Write a query to get the average salary for all departments employing more than 10 employees.
+select department_id,avg(salary),count(`EMPLOYEE_ID`) from employees  GROUP BY department_id having count(`EMPLOYEE_ID`)>10;
 
 
 
@@ -1230,5 +1259,27 @@ select * from employees where last_name like '%BLAKE%' or last_name like '%SCOTT
 
 
 
-6. Write a query to display the last name, job, and salary for all employees whose job is that of a Programmer or a Shipping Clerk, and whose salary is not equal to $4,500, $10,000, or $15,000
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
